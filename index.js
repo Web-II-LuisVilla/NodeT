d633bb040df853f83231138ed03c7b924bc9c4ba
luis eduardo villa ojeda <112597598+lvilla8@users.noreply.github.com>
2023-09-10 14:13:34 -0500

    Comit1

---
+++ index.js
@@ -0,0 +1,42 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+
+const fs = require("fs");
+// const path = require("path");
+// const bodyParser = require("body-parser");
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/enviar", (req, res) => {
+    console.log(req.body)
+
+  const { id, nombre, apellido, titulo, autor, editorial, año } = req.body;
+
+  if (!id || !nombre || !apellido || !titulo || !autor || !editorial || !año) {
+    return res.redirect("/error.html");
+  }
+
+  const contentFile = `Id: ${id}\nNombre: ${nombre}\nApellido: ${apellido}\nTitulo: ${titulo}\nAutor: ${autor}\nEditorial: ${editorial}\nAño: ${año}`
+
+  const nameFile = `data/id_${id}.txt`;
+
+  fs.writeFile(nameFile, contentFile, (err) => {
+    if (err) {
+      console.error(err);
+      return res.redirect('/error.html');
+    }
+
+    res.download(nameFile, (err) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).send("Error al descargar el archivo");
+      }
+    });
+  });
+});
+
+app.listen(port, () => console.log("funcionando", port));
