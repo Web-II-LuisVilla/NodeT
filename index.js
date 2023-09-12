cd4f05176ec3e1e85fc875d9f6bcdd1b763c9995
luis eduardo villa ojeda <112597598+lvilla8@users.noreply.github.com>
2023-09-11 21:49:20 -0500

    Final

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-const bodyParser = require("body-parser");
 const express = require("express");
 
 const fs = require("fs");
-// const path = require("path");
-// const bodyParser = require("body-parser");
+const bodyParser = require("body-parser");
 
 const app = express();
 const port = 3000;
